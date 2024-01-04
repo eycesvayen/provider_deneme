f4fe02370aea24ffc509a9e2bb07d78919e9bda9
eycesvayen <ozgurhsyn@gmail.com>
2024-01-04 23:38:17 +0300

    first commit

---
+++ dunya_ulkeleri/lib/view/main_page.dart
@@ -0,0 +1,36 @@
+import 'package:provider_deneme/model/country.dart';
+import 'package:provider_deneme/view_model/main_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ülkeler"),
+      ),
+      body: _buildCountryList(context),
+    );
+  }
+
+  Widget _buildCountryList(BuildContext context) {
+    return Consumer<MainViewModel>(
+      builder: (context, viewModel, child) => ListView.builder(
+        itemCount: viewModel.countries.length,
+        itemBuilder: (context, index) {
+          Country country = viewModel.countries[index];
+          return ListTile(
+            title: Text(country.name),
+            subtitle: Text(country.capital),
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(country.flag),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
